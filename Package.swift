1fabb11811a4bbfca48544bcc8cbc6f7de9f715e
Luke Howard <lukeh@padl.com>
2025-03-14 16:41:17 +1100

    add tests

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/PADL/SwiftOCA", branch: "main"),
-    .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "4.0.0"),
+    .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"4.0.0"),
     .package(url: "https://github.com/apple/swift-system", from: "1.2.1"),
   ],
   targets: [
@@ -30,8 +30,14 @@ let package = Package(
       dependencies: [
         "SwiftOCA",
         .product(name: "Crypto", package: "swift-crypto"),
-        .product(name: "SystemPackage", package: "swift-system")
+        .product(name: "SystemPackage", package: "swift-system"),
       ]
-    )
+    ),
+    .testTarget(
+      name: "OcaFirmwareImageContainerTests",
+      dependencies: [
+        "OcaFirmwareImageContainer",
+      ]
+    ),
   ]
 )
